30731d74f6cce9b7d706783b28a715424eb6c35f
sabrinatouch <sabrinajindatouch@gmail.com>
2023-12-20 18:14:25 -0700

    Reorganize files

---
+++ pages/aboutme.js
@@ -0,0 +1,22 @@
+import {
+    Box,
+    Text,
+    Heading
+} from "@chakra-ui/react"
+
+const AboutMe = () => {
+    return (
+        <Box>
+            <Heading variant="section-title">
+                About Me
+            </Heading>
+            <Box>
+                <Text>
+                    
+                </Text>
+            </Box>
+        </Box>
+    )
+}
+
+export default AboutMe
