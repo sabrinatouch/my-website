7f61f52ac037d44299d3308587f28d28c5ebf894
sabrinatouch <sabrinajindatouch@gmail.com>
2024-02-02 16:34:51 -0700

    Complete Button functionality, transitions, and animations

--- components/Header/Button/index.jsx
+++ components/Header/Button/index.jsx
@@ -12,12 +12,23 @@ export default function index({isActive, setIsActive}) {
                 transition={{ duration: 0.5, ease: [0.76, 0, 0.24, 1] }}
             >
                 <div className={styles.el}>
+                    <PerspectiveText label="Menu" />
                     <p>Menu</p>
                 </div>
                 <div className={styles.el}>
+                    <PerspectiveText label="Close" />
                     <p>Close</p>
                 </div>
             </motion.div>
+        </div>
+    )
+}
+
+function PerspectiveText({label}){
+    return (
+        <div className={styles.perspectiveText}>
+            <p>{label}</p>
+            <p>{label}</p>
         </div>
     )
 }
