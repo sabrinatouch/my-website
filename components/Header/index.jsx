3f893bec8bae59712622af2eafd130be34858ae2
sabrinatouch <sabrinajindatouch@gmail.com>
2024-02-02 16:53:44 -0700

    Implement menu div, transition, animation on button click

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -4,6 +4,18 @@
 import React, { useState } from 'react'
 import Button from './Button'
 import styles from './style.module.scss'
+import { motion } from 'framer-motion'
+
+const variants = {
+    open: {
+        width: 480,
+        height: 650
+    },
+    closed: {
+        width: 100,
+        height: 40
+    }
+}
 
 export default function index() {
     // Creating a state to track if the menu is open or close
@@ -11,6 +23,15 @@ export default function index() {
 
     return (
         <div className={styles.header}>
+            <motion.div 
+                className={styles.menu}
+                variants={variants}
+                animate={isActive ? "open" : "closed"}
+                initial="closed"
+            >
+
+            </motion.div>
+
             <Button isActive={isActive} setIsActive={setIsActive} />
         </div>
     )
