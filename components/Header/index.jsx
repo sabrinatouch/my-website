ab64e37c52cbfee50ee7ee227a2bfa2a4431d1e6
sabrinatouch <sabrinajindatouch@gmail.com>
2024-02-02 16:59:37 -0700

    Add simple transition to menu open/close

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -12,13 +12,15 @@ const variants = {
         height: 650,
         // for these top, right properties to work, make sure to add position: relative in scss
         top: "-25px",
-        right: "-25px"
+        right: "-25px",
+        transition: {duration: 0.65, ease: [0.76, 0, 0.24, 1]}
     },
     closed: {
         width: 100,
         height: 40,
         top: "0px",
-        right: "0px"
+        right: "0px",
+        transition: {duration: 0.65, ease: [0.76, 0, 0.24, 1]}
     }
 }
 
