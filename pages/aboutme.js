5a71d27de1b50db0a4374b4ac9807d71fee8aa49
sabrinatouch <sabrinajindatouch@gmail.com>
2024-02-19 19:57:08 -0700

    About me page work

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -4,29 +4,34 @@ import {
     Heading,
     VStack,
     SimpleGrid,
-    Image
+    Image,
+    AspectRatio
 } from "@chakra-ui/react"
 import TransitionAnimationRight from "../components/transitionanimationright"
 
 const AboutMe = () => {
     return (
         <Box>
-            <Heading variant="section-title" display={{ base: 'none', lg: 'flex' }}>
-                About Me
-            </Heading>
-            <Heading variant="sub-title" display={{ base: 'flex', lg: 'none' }}>
-                About Me
-            </Heading>
             <Box mt={4}>
-                <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={8}>
-                    <Text variant="sub-title">
-                        From custom websites to crafting your ideal logo, I translate imagination into beautiful digital visuals.
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8}>
+                    <Box display={{ base: 'relative', lg: 'flex' }} alignItems="flex-start" justifyContent="center" flexDirection="column">
+                        <Heading variant="section-title" display={{ base: 'none', lg: 'flex' }}>
+                            About Me
+                        </Heading>
+                        <Heading variant="sub-title" display={{ base: 'flex', lg: 'none' }}>
+                            About Me
+                        </Heading>
+                        <Text mt={4} variant="sub-title"> 
+                            From custom websites to crafting your ideal logo, I translate imagination into beautiful digital visuals.
 
-                        As a Cambodian-American creative, I especially love combining culture and heritage into my work.
-                    </Text>
+                            As a Cambodian-American creative, I especially love combining culture and heritage into my work.
+                        </Text>
+                    </Box>
                     <Box>
                         <TransitionAnimationRight>
-                            <Image src={'./selfie.png'} borderRadius='15px' />
+                        
+                                <Image src={'./selfie.png'} borderRadius='15px' />
+                     
                         </TransitionAnimationRight> 
                     </Box>
                 </SimpleGrid>
