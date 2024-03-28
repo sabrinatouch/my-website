a4b5dd23419cd68b7c11cba41e4e7bedba17a058
sabrinatouch <sabrinajindatouch@gmail.com>
2024-03-28 13:07:50 -0600

    Added in more works

--- pages/works2.js
+++ pages/works2.js
@@ -19,6 +19,8 @@ const Works2 = () => {
         </Heading>
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={14}>
           <WorkItem2 id="bobatouch" href="https://bobatouch.com/" thumbnail="/tb-bobatouch.png" title="Boba Touch" description="Website" />
+          <WorkItem2 id="personalwebsite" href="/" thumbnail="/thumbnail_placeholder.png" title="Personal Website" description="Website" />
+          <WorkItem2 id="3dmodel" href="/" thumbnail="/thumbnail_placeholder.png" title="3D Desk Model" description="3D Modeling" />
           <WorkItem2 id="khmerconference" href="/" thumbnail="/tb-khmerconference.png" title="Cambodian Networking Conference" description="Graphic Design" />
         </SimpleGrid>
       </Section>
