49b3e5751aa8add4358b9931a397f0b37abffe58
sabrinatouch <sabrinajindatouch@gmail.com>
2024-10-28 14:39:17 -0600

    Fix punctuation errors

--- pages/works/cambodianlogo.js
+++ pages/works/cambodianlogo.js
@@ -46,7 +46,7 @@ const Work = () => {
                     </Heading>
                     <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: '6', md: '6' }}>
                         <Text>
-                            Khmer &#40;Cambodian&#41; kbach refers to traditional decorative art and ornamental patterns that are used in architecture, sculpture, painting, and various forms of craftwork. The term "kbach" ក្បាច់  means "style" or "pattern" in Khmer, and it is a deeply significant part of Cambodian cultural heritage, often used to adorn temples, royal buildings, and religious objects. Kbach motifs are highly stylized and symbolic, often drawing inspiration from nature and celestial shapes symbolizing harmony, balance, and spirtuality.
+                            Khmer (Cambodian) kbach refers to traditional decorative art and ornamental patterns that are used in architecture, sculpture, painting, and various forms of craftwork. The term &quot;kbach&quot; ក្បាច់  means &quot;style&quot; or &quot;pattern&quot; in Khmer, and it is a deeply significant part of Cambodian cultural heritage, often used to adorn temples, royal buildings, and religious objects. Kbach motifs are highly stylized and symbolic, often drawing inspiration from nature and celestial shapes symbolizing harmony, balance, and spirtuality.
                         </Text>
                         <Text>
                             The krama is a traditional and versatile scarf that holds profound cultural significance in Cambodia. Typically made from cotton or silk and featuring a checkered pattern in various colors, the krama serves many purposes: it offers protection from dust and sun, is used to carry goods, and can even be given as a gift of honor, much like a Hawaiian lei. More than just a practical garment, it symbolizes Cambodian identity, heritage, and way of life used by people of all ages and walks of life.
@@ -67,10 +67,10 @@ const Work = () => {
                     </Heading>
                     <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: '6', md: '6' }}>
                         <Text>
-                            PP Agrandir was selected as the primary typeface for its visual similarity to one of Cambodia&#39;s iconic landmarks&#44; Angkor Wat. The wide&#44; stretched form of the characters mirrors the temple&#39;s horizontal grandeur&#44; while the combination of curves and straight lines reflects the intricate details of its architecture.
+                            PP Agrandir was selected as the primary typeface for its visual similarity to one of Cambodia&apos;s iconic landmark, Angkor Wat. The wide, stretched form of the characters mirrors the temple&apos;s horizontal grandeur, while the combination of curves and straight lines reflects the intricate details of its architecture.
                         </Text>
                         <Text>
-                            Additionally&#44; the letter &#39;R&#39; was creatively merged with the Khmer character &#39;រ&#39;&#44; which begins the Khmer word for &#39;rise&#39; &#40;រហ&#41;&#44; further emphasizing cultural relevance. Noto Sans Khmer was chosen for the Khmer script&#44; as its font weight aligns seamlessly with PP Agrandir&#44; creating a cohesive and streamlined typographic design.
+                            Additionally, the letter &quot;R&quot; was creatively merged with the Khmer character &quot;រ&quot; which begins the Khmer word for &quot;rise&quot; (រហ) further emphasizing cultural relevance. Noto Sans Khmer was chosen for the Khmer script, as its font weight aligns seamlessly with PP Agrandir, creating a cohesive and streamlined typographic design.
                         </Text>
                     </SimpleGrid>
                 </Box>
@@ -94,7 +94,7 @@ const Work = () => {
                             Colors
                         </Heading>    
                         <Text pb={16}>
-                            Creating a custom color palette offered limitless possibilities. My goal was to design a palette that both evoked the essence of Cambodian culture and introduced a fresh, modern feel. Orange was chosen as the primary color to symbolize the brand’s themes of rise and match the fire symbolism of the logo. The secondary color, a deep teal, reflects Cambodia’s lush jungles and nature, while also adding a sense of richness, symbolizing the country’s deep history. This dark teal serves as a backdrop that allows the orange to stand out and glow.
+                            Creating a custom color palette offered limitless possibilities. My goal was to design a palette that both evoked the essence of Cambodian culture and introduced a fresh, modern feel. Orange was chosen as the primary color to symbolize the brand&apos;s themes of rise and match the fire symbolism of the logo. The secondary color, a deep teal, reflects Cambodia&apos;s lush jungles and nature, while also adding a sense of richness, symbolizing the country&apos;s deep history. This dark teal serves as a backdrop that allows the orange to stand out and glow.
                         </Text>
                         <Image
                             src="/final8.jpg"
