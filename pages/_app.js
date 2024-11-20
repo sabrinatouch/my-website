a18dac376b8d2a0cd1ff634177458ca877f38deb
sabrinatouch <sabrinajindatouch@gmail.com>
2024-11-20 15:45:58 -0700

    Relocate viewport meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import Chakra from '../components/chakra'
 import Fonts from '../components/fonts'
+import Head from 'next/head'
 
 function Website({ Component, pageProps, router }) {
     return (
         <Chakra>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
             <Fonts />
             <Component {...pageProps} />
         </Chakra>
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,6 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="shortcut icon" href="favicon.png" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <body>
                 <Main />
